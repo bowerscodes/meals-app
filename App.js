45532d700d5265c6c77d9004a2eb248028255403
James Bowers <jamesbowers123@gmail.com>
2024-07-16 20:06:24 +0100

    add Drawer navigation

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 // Local imports
+import Favourites from './screens/Favourites';
 import CategorySelect from './screens/CategorySelect';
 import MealsOverview from './screens/MealsOverview';
 import MealDetails from './screens/MealDetails';
@@ -12,6 +14,23 @@ import FavouritesContextProvider from './store/context/favourites-context';
 
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen 
+        name='AllCategories'
+        component={CategorySelect}
+        options={{title: 'Categories'}}
+      />
+      <Drawer.Screen 
+        name='Favourites'
+        component={Favourites}
+      />
+    </Drawer.Navigator>
+  )
+};
 
 export default function App() {
   return (
@@ -19,9 +38,18 @@ export default function App() {
       <StatusBar style='auto' />
       <FavouritesContextProvider>
         <NavigationContainer
-          theme={{colors: {background: 'lightgray'}}}
+          // theme={{colors: {background: 'lightgray'}}}
         >
-          <Stack.Navigator initialRouteName='CategorySelect'>
+          <Stack.Navigator 
+            // initialRouteName='CategorySelect'
+          >
+            <Stack.Screen
+              name='Drawer'
+              component={DrawerNavigator} 
+              options={{
+                headerShown: false
+              }}
+            />
             <Stack.Screen 
               name='CategorySelect' 
               component={CategorySelect} 
---
+++ screens/Favourites.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+
+const Favourites = () => {
+
+  return (
+    <Text>Favourites</Text>
+  );
+}
+
+export default Favourites;
